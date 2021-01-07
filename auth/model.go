84efc10f76aac65cd8ab3b695fb1b05448999724
彭博 <pengbo@dustess.com>
2021-01-07 11:50:20 +0800

    feat: 🎸 登录授权信息补全

--- auth/model.go
+++ auth/model.go
@@ -1,5 +1,10 @@
 package auth
 
+const (
+	LoginAuth  = "loginAuth"  // 用户登录授权
+	AccessAuth = "accessAuth" // 访问授权
+)
+
 // 基础用户信息
 type BaseAuth struct {
 	UserID     string `json:"userId"`     // 用户ID
@@ -7,6 +12,7 @@ type BaseAuth struct {
 	Source     string `json:"source"`     // 授权来源
 	CreateTime string `json:"createTime"` // 授权时间
 	Role       string `json:"role"`       // 角色
+	AuthType   string `json:"authType"`   // 授权类型
 }
 
 // 用户授权信息补全
