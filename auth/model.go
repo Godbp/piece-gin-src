9de89b533d3009ed6530f1a15d28892ca64b607e
彭博 <pengbo@dustess.com>
2021-01-07 11:46:51 +0800

    feat: 🎸 修改用户授权信息

--- auth/model.go
+++ auth/model.go
@@ -1,9 +1,15 @@
 package auth
 
-type Auth struct {
+// 基础用户信息
+type BaseAuth struct {
 	UserID     string `json:"userId"`     // 用户ID
 	UserName   string `json:"userName"`   // 用户名称
 	Source     string `json:"source"`     // 授权来源
 	CreateTime string `json:"createTime"` // 授权时间
 	Role       string `json:"role"`       // 角色
+}
+
+// 用户授权信息补全
+type Auth struct {
+	BaseAuth
 }
